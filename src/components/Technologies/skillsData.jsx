ad06d6208c6eeaecb2afd8bb5955b954c2c1fb2d
rxmxndai <k.romann413@gmail.com>
2022-09-17 22:12:01 +0545

    Skills page on hover shows details

--- src/components/Technologies/skillsData.jsx
+++ src/components/Technologies/skillsData.jsx
@@ -2,34 +2,41 @@
 export const featuredSkills = [
     {   
       id: 1,
-      img: require("../../assets/javascript.png")
+      img: require("../../assets/javascript.png"),
+      title: "JavaScript"
     },
     {   
       id: 2,
-      img: require("../../assets/node.png")
+      img: require("../../assets/node.png"),
+      title: "Node JS"
     },
     {   
       id: 3,
-      img: require("../../assets/react.png")
+      img: require("../../assets/react.png"),
+      title: "React"
     },
     {   
       id: 4,
-      img: require("../../assets/figma.png")
+      img: require("../../assets/figma.png"),
+      title: "Figma"
     }
   ];
   
   export const Frontend = [
     {
       id: 1,
-      img: require("../../assets/react.png")
+      img: require("../../assets/react.png"),
+      title: "React"
     },
     {
       id: 2,
-      img: require("../../assets/html.png")
+      img: require("../../assets/html.png"),
+      title: "HTML5"
     },
     {
       id: 3,
-      img: require("../../assets/css.png")
+      img: require("../../assets/css.png"),
+      title: "CSS3"
     }
   ];
   
@@ -37,16 +44,19 @@ export const featuredSkills = [
   export const Languages = [
     {
       id: 1,
-      img: require("../../assets/javascript.png")
+      img: require("../../assets/javascript.png"),
+      title: "JavaScript"
 
     },
     {
       id: 2,
-      img: require("../../assets/cpp.png")
+      img: require("../../assets/cpp.png"),
+      title: "C++ (DSA)"
     },
     {
       id: 3,
-      img: require("../../assets/java.png")
+      img: require("../../assets/java.png"),
+      title: "Java (oop)"
     },
 
   ];
@@ -54,11 +64,13 @@ export const featuredSkills = [
   export const UIUX = [
     {
       id: 1,
-      img: require("../../assets/figma.png")
+      img: require("../../assets/figma.png"),
+      title: "Figma"
     },
     {
       id: 2,
-      img: require("../../assets/balsamiq.png")
+      img: require("../../assets/balsamiq.png"),
+      title: "Balsamiq"
     }
   ];
 
@@ -66,11 +78,13 @@ export const featuredSkills = [
   export const Backend = [
     {
       id: 1,
-      img: require("../../assets/node.png")
+      img: require("../../assets/node.png"),
+      title: "Node JS"
 
     },
     {
       id: 2,
-      img: require("../../assets/mongodb.png")
+      img: require("../../assets/mongodb.png"),
+      title: "Mongo DB"
     },
   ]
